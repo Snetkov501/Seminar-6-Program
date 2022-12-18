b1256c16050ebdac4f9da442e0d453c63a689c8a
SnetkovAN <monkefak@mail.ru>
2022-12-18 21:43:08 +0600

    seminar6

---
+++ C# S6 Task 41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task 41
+
+Console.Clear();
+
+Console.Write("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Дружище, количество элементов больше 0: {count}");
